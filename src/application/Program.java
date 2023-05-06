d0f4b633723097c5f9d3428dc3f11259ade94be5
Nog <lucas.nogueiramartins@gmail.com>
2023-05-06 02:21:53 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,13 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
 		
+		
+		System.out.println("\n==== TEST 5: Seller Insert ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
+		
 	}
 
 }
