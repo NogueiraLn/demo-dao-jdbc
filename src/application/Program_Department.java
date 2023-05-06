f735ed076db0907e2fe5a4a385118166c310747a
Nog <lucas.nogueiramartins@gmail.com>
2023-05-06 05:08:12 -0300

    Starting DepartmentDaoJDBC / Department Test - findById & findAll implementation / 2

---
+++ src/application/Program_Department.java
@@ -0,0 +1,23 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program_Department {
+
+	public static void main(String[] args) {
+	
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("==== TEST 1: Department findById ====");
+		Department department = departmentDao.findById(1);
+		System.out.println(department);
+		
+		
+		
+		
+
+	}
+
+}
